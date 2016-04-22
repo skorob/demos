aff7370882a505f70ccd4ab26b7edd5b3fa43963
Oleg Skorobogagtov <os@ipr.lv>
2016-04-22 11:12:17 +0300

    Cycling bareer

---
+++ java/SpringIntegrationDemo/src/main/java/lv/oskor/springint/TempConverter.java
@@ -0,0 +1,9 @@
+package lv.oskor.springint;
+
+/**
+ * Created by Oleg Skorobogatov on 05.04.2016.
+ */
+public interface TempConverter {
+
+    float fahrenheitToCelcius(float fahren);
+}
--- java/SpringIntegrationDemo/src/main/java/lv/oskor/springint/test1.java
+++ java/SpringIntegrationDemo/src/main/java/lv/oskor/springint/test1.java
@@ -1,7 +1,23 @@
 package lv.oskor.springint;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Oleg Skorobogatov on 05.04.2016.
  */
-public class test1 {
+public class Test1 {
+
+
+        public static void main(String... args) throws Exception {
+            ApplicationContext ctx =
+                new ClassPathXmlApplicationContext("context.xml");
+            // Simple Service
+            TempConverter converter =
+                ctx.getBean("simpleGateway", TempConverter.class);
+            System.out.println(converter.fahrenheitToCelcius(68.0f));
+            // Web Service
+            converter  = ctx.getBean("wsGateway", TempConverter.class);
+            System.out.println(converter.fahrenheitToCelcius(68.0f));
+        }
 }
